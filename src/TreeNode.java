2a0850aa1a25925212bdf1f0efc436987a4813e8
km4891 <kmorarity@gmail.com>
2021-10-19 21:44:58 -0700

    added treenode

---
+++ src/TreeNode.java
@@ -0,0 +1,21 @@
+public class TreeNode {
+    char nodechar;
+	static int size;
+	int frequency;
+	StringBuffer code;
+	TreeNode left,right;
+	
+    TreeNode(char ch,int fr){
+		nodechar=ch;
+		frequency=fr;
+		size++;
+		left=right=null;
+	}
+	TreeNode(char ch,int fr,TreeNode left,TreeNode right){
+		nodechar=ch;
+		frequency=fr;
+		size++;
+		this.left=left;
+		this.right=right;
+	}
+}
