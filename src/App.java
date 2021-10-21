44f74391420ab26869057a7c3ada833dfb905481
km4891 <kmorarity@gmail.com>
2021-10-20 21:12:13 -0700

    start over

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    //for all ASCII characters
+    private static final int ALPHABET_SIZE= 256;
+
+    public HuffmanEncodedResult compress (final String data){
+        final int [] freq = new int[ALPHABET_SIZE];
+        for(final char character : data.toCharArray()) {
+            freq[character]++; //characters can be treated like integers
+
+        }
+        
+        return null;
     }
+
+    public String decompress (final HuffmanEncodedResult result) {
+        return null;
+    }
+
+    static class HuffmanEncodedResult {
+
+    }
+	public static void main(String[] args) {
+	}
+
 }
